67e48b8782267533e2cc0a8d7a8bf20da5f5b037
1babo <1babo@proton.me>
2023-09-01 14:09:33 -0700

    leather 4.14.0

--- Casks/leather.rb
+++ Casks/leather.rb
@@ -1,21 +1,22 @@
 cask "leather" do
-  version "4.10.1"
-  sha256 "0ed51289872b97e39e8f1e934f67353cd7e9e0b92500f336e3114065860d2594"
+  version "4.14.0"
+  sha256 "e6a357517cdca5f4701f423e9c3e59e6a88a5042fc63b89f25685077f2e4ecf2"
 
-  url "https://github.com/hirosystems/desktop-wallet/releases/download/v#{version}/stacks-wallet.mainnet.v#{version}.dmg",
-      verified: "github.com/hirosystems/desktop-wallet/"
-  name "Hiro Wallet"
+  url "https://github.com/leather-wallet/desktop/releases/download/v#{version}/leather-wallet.mainnet.v#{version}.dmg",
+      verified: "github.com/leather-wallet/desktop/"
+  name "Leather Wallet"
   desc "Desktop wallet for Stacks blockchain"
-  homepage "https://wallet.hiro.so/"
+  homepage "https://leather.io/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  app "Hiro Wallet.app"
+  app "Leather.app"
 
   zap trash: [
+    "~/Library/Application Support/Leather",
     "~/Library/Application Support/so.hiro.StacksWallet",
     "~/Library/Preferences/so.hiro.StacksWallet.plist",
     "~/Library/Saved Application State/so.hiro.StacksWallet.savedState",
