15437bce9902a08d97a7290396684e2aaeccdfcd
1babo <1babo@proton.me>
2023-09-01 11:30:44 -0700

    rabby-desktop: web3 wallet (new cask)

---
+++ Casks/rabby-desktop.rb
@@ -0,0 +1,25 @@
+cask "rabby-desktop" do
+  arch arm: "arm64", intel: "x64"
+
+  version :latest
+  sha256 :no_check
+
+  url "https://download.rabby.io/wallet-desktop/darwin-#{arch}/rabby-wallet-desktop-installer-#{arch}-latest.dmg"
+  name "Rabby Desktop"
+  desc "Desktop wallet for Ethereum and EVM chains"
+  homepage "https://rabby.io/"
+
+  livecheck do
+    # strategy :extract_plist
+    skip "No version information available"
+  end
+
+  app "Rabby Desktop.app"
+
+  zap trash: [
+    "~/Library/Application Support/rabby-desktop",
+    "~/Library/Logs/rabby-desktop",
+    "~/Library/Preferences/com.debank.RabbyDesktop.plist",
+    "~/Library/Saved Application State/com.debank.RabbyDesktop.savedState",
+  ]
+end
