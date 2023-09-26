dc3bdd2f43d678bd5cd62bcc5181b5acf077a233
git <1babo@proton.me>
2023-09-26 09:28:40 -0700

    rabby-desktop 0.34.0

--- Casks/rabby-desktop.rb
+++ Casks/rabby-desktop.rb
@@ -1,7 +1,7 @@
 cask "rabby-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.33.0"
+  version "0.34.0"
   sha256 :no_check
 
   url "https://download.rabby.io/wallet-desktop/darwin-#{arch}/rabby-wallet-desktop-installer-#{arch}-latest.dmg"
