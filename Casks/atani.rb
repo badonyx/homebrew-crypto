ae618703b41d99cbfd5e3d24d764238098db9761
bad <badonyx@proton.me>
2024-08-28 20:21:22 -0700

    atani 14.48.0

--- Casks/atani.rb
+++ Casks/atani.rb
@@ -1,9 +1,9 @@
 cask "atani" do
   arch arm: "-arm64"
 
-  version "14.47.0"
-  sha256 arm:   "9f8ebb428b8d2ab9cc6bf6f68634bf68e273f395b6d9f1370a101328646e2582",
-         intel: "8427d2bebd300261205d45aaa638e372de1a1aedddcf3e18469fd0d2fa46809a"
+  version "14.48.0"
+  sha256 arm:   "1bd7e0e8a149fa648198b4208c356eae7503f3ed1aa8e74b22c1fc4338cb2bfe",
+         intel: "fa81e10e591121aa46ee750018cb1661f116590ce6e3d0c9155c33609d924f52"
 
   url "https://cdn.atani.com/desktop/releases/Atani-#{version}#{arch}.dmg"
   name "Atani"
