29fb4f8a1bf9c2b5b495f316814cfcefe322e2df
bad <badonyx@proton.me>
2026-01-02 23:00:55 +0000

    bitcoin-knots 29.2.knots20251110

--- Casks/bitcoin-knots.rb
+++ Casks/bitcoin-knots.rb
@@ -1,15 +1,17 @@
 cask "bitcoin-knots" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "27.1.knots20240801"
-  sha256 arm:   "030587371794f02c1f847b28638e139e9c3ceedf6512b2fdab5c8500e8ea01e3",
-         intel: "ec33a8a9e66c3d0aa6738cb72853526f5949e819d69e695aff5138916adf4243"
+  version "29.2.knots20251110"
+  sha256 arm:   "db162ae8b6198278da6657d00eddf6f642fd5dccdbc32b124fad6a26e6f4ece1",
+         intel: "a736c6a64f4e300ef0cda8d0106f341daad86fb1012eeeaf3e5136c556eba660"
 
   url "https://github.com/bitcoinknots/bitcoin/releases/download/v#{version}/bitcoin-#{version}-#{arch}-apple-darwin.zip",
       verified: "github.com/bitcoinknots/bitcoin/"
   name "Bitcoin Knots"
   desc "Enhanced Bitcoin node/wallet software"
   homepage "https://bitcoinknots.org/"
+
+  depends_on macos: ">= :ventura"
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   app "Bitcoin-Qt.app", target: "Bitcoin Knots.app"
