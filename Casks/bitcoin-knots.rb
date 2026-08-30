cea80b02b294b90c6dc6d8e10f7a120439eedf82
bad <badonyx@proton.me>
2026-08-30 13:15:03 -0700

    bitcoin-knots 29.4.knots20260508

--- Casks/bitcoin-knots.rb
+++ Casks/bitcoin-knots.rb
@@ -1,9 +1,9 @@
 cask "bitcoin-knots" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "29.2.knots20251110"
-  sha256 arm:   "db162ae8b6198278da6657d00eddf6f642fd5dccdbc32b124fad6a26e6f4ece1",
-         intel: "a736c6a64f4e300ef0cda8d0106f341daad86fb1012eeeaf3e5136c556eba660"
+  version "29.4.knots20260508"
+  sha256 arm:   "4218c01abf01aad7086dc4a1c332c0ea3e110aacf65dc6bc7e1f35d443725acb",
+         intel: "c054aef2142be9cbb247515bb2cdc302672d7676ab81a5e91cfe8f61271534d1"
 
   url "https://github.com/bitcoinknots/bitcoin/releases/download/v#{version}/bitcoin-#{version}-#{arch}-apple-darwin.zip",
       verified: "github.com/bitcoinknots/bitcoin/"
